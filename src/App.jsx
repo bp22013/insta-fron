c8e3e9d52c82fb0b7573d2d95a2c3c5f38b5ebdc
bp22013 <118529334+bp22013@users.noreply.github.com>
2024-03-05 01:20:41 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+    const [userId, setUserId] = useState("");
+    const [userInfo, setUserInfo] = useState(null);
+    const url = "https://insta-sc-7r18.onrender.com";
+
+    const handleChange = (event) => {
+        setUserId(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post(url, { user_id: userId });
+            setUserInfo(response.data);
+        } catch (error) {
+            console.error("Error fetching user info:", error);
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    検索したいユーザーのIDを入力：
+                    <input type="text" value={userId} onChange={handleChange} />
+                </label>
+                <button type="submit">検索</button>
+            </form>
+            {userInfo && (
+                <div>
+                    <h2>ユーザー情報</h2>
+                    <p>ユーザー名: {userInfo.username}</p>
+                    <p>フォロワー数: {userInfo.followers}</p>
+                    <p>フォロー数: {userInfo.following}</p>
+                    <p>バイオグラフィ: {userInfo.biography}</p>
+                    {/* 画像の表示 */}
+                    <img src={userInfo.profile_pic_url} alt="Profile Pic" />
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default App;
